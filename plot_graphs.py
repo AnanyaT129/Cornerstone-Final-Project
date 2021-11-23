71672d3b5bc02a219718a0f76d33757440854f58
kaitlynramesh <68577310+kaitlynramesh@users.noreply.github.com>
2021-11-22 21:33:50 -0500

    Add files via upload
    basic graphs for incoming data, RMS, FFT, and median frequency (median frequency graph needs more work or better data)

---
+++ plot_graphs.py
@@ -0,0 +1,83 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+from scipy import integrate
+from scipy import signal
+from matplotlib.backends.backend_pdf import PdfPages
+
+# open data file
+with open('output.txt') as f:
+    voltage_list = f.read()
+
+voltage_list = voltage_list[1:len(voltage_list)-2]
+voltage_list = voltage_list.split(',')
+voltage_list = list(map(float, voltage_list))
+
+# analysis sampling rate, frequencies, etc. 
+N = len(voltage_list)
+samples = range(N)
+Fs = 40; # in Hz
+Fnyq = Fs / 2
+freqs = np.arange(0, Fnyq, Fs/N)
+
+# GRAPH 1: Voltage vs. Samples (Myoware Output)
+ax = plt.subplot(2, 2, 1)
+ax.set_ylim([0,6000])
+ax.set_xlim([0, 500])
+plt.plot(samples, voltage_list, '-k')
+ax.title.set_text('Myoware sEMG Data')
+ax.set_ylabel('Voltage (mV)') 
+ax.set_xlabel('Samples')
+
+# GRAPH 2: RMS vs. Samples
+# RMS to smooth out data
+squared_data = [0] * N
+rms = [0] * N
+for i in range(0, N):
+     squared_data[i] = voltage_list[i] ** 2
+rms_squared = pd.Series(squared_data).rolling(window=15).mean()
+rms = np.sqrt(rms_squared)
+
+rms_ax = plt.subplot(2, 2, 2)
+rms_ax.set_ylim([0, 6000])
+rms_ax.set_xlim([0, 500])
+plt.plot(samples, rms, '-b')
+rms_ax.title.set_text('RMS of sEMG Data')
+rms_ax.set_ylabel('Voltage (mV)') 
+rms_ax.set_xlabel('Samples')
+
+# GRAPH 3: Discrete Fourier Transform --> Frequency Domain
+fft_ax = plt.subplot(2, 2, 3)
+volt_fft = np.fft.fft(voltage_list-np.mean(voltage_list)) # filtering out mean
+i = range(int(N/2)+1)
+
+volt_fft = volt_fft[i]
+fft_ax.set_xlim([0, Fnyq])
+plt.plot(freqs, abs(volt_fft)*2, '-b')
+fft_ax.title.set_text('Fourier Analysis of sEMG Data')
+fft_ax.set_ylabel('Amplitude') 
+fft_ax.set_xlabel('Frequency (Hz)')
+
+# GRAPH 4: Median Frequency over Time - kind of sketchy atm
+freq, psd = signal.periodogram(voltage_list, fs = Fs)
+psd_per_sec = np.array_split(psd, int(len(psd) / (N/Fs))) # break PSD into arrays containing 1s of data
+medfreq = []
+
+for power in psd_per_sec:
+    
+    freq_per_sec = np.arange(0, Fnyq, Fnyq/len(power)) 
+    area_freq = integrate.cumtrapz(power, freq_per_sec, initial=0) # cumulative power at each frequency
+    total_power = area_freq[-1] 
+    medfreq.append([freqs[np.where(area_freq >= total_power / 2)[0][0]]]) # medfreq located where power/2
+
+med_ax = plt.subplot(2, 2, 4)
+t = np.arange(len(medfreq))
+plt.plot(t, medfreq, 'o') # need to figure out why len(medfreq) != 16...
+m, b = np.polyfit(t, medfreq, 1) # returns slope of median frequency <- can use this for..more analysis :,)
+plt.plot(t, m*t + b, 'g')
+med_ax.title.set_text('Median Frequency Over Time')
+med_ax.set_ylabel('Frequency (Hz)') 
+med_ax.set_xlabel('Time (s)') 
+
+plt.show()
+
